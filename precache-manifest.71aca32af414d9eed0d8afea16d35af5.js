3cf66600cdfa936e3e1a7ff841383fa04090ae4d
antoninabagrin <antonia_bagrin@yahoo.com>
2021-12-29 19:42:38 +0100

    Updates

---
+++ precache-manifest.71aca32af414d9eed0d8afea16d35af5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "828bf4a5cf4443a87e3c",
+    "url": "/robofriends/static/css/main.d77c6958.chunk.css"
+  },
+  {
+    "revision": "828bf4a5cf4443a87e3c",
+    "url": "/robofriends/static/js/main.03e059b7.chunk.js"
+  },
+  {
+    "revision": "e273694afe77556ed6a0",
+    "url": "/robofriends/static/js/runtime~main.884587fe.js"
+  },
+  {
+    "revision": "09c3e068a6b6cf59093e",
+    "url": "/robofriends/static/css/2.eab18348.chunk.css"
+  },
+  {
+    "revision": "09c3e068a6b6cf59093e",
+    "url": "/robofriends/static/js/2.9e12874e.chunk.js"
+  },
+  {
+    "revision": "510535223dcd279f3967",
+    "url": "/robofriends/static/js/3.e9d442d7.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a38a1786543c67a06645ad5b03382e83",
+    "url": "/robofriends/index.html"
+  }
+];
